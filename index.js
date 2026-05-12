7151fb43c9f1361d79b9a01acf1aff1455fe09cd
Mostafa <mostafayasser0859@gmail.com>
2026-05-12 04:08:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-﻿const sdk = require('node-appwrite');
+const sdk = require('node-appwrite');
 
 module.exports = async function (context) {
     const client = new sdk.Client();
     const users = new sdk.Users(client);
 
-    // ربط الفنكشن بمشروعك باستخدام المتغيرات اللي ضفناها في الـ Settings
+    // ربط الفنكشن بمشروعك
     client
         .setEndpoint('https://cloud.appwrite.io/v1')
         .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
@@ -14,29 +14,18 @@ module.exports = async function (context) {
     try {
         payload = JSON.parse(context.req.body || '{}');
     } catch (e) {
-        return context.res.json({ success: false, message: 'بيانات غير صالحة' });
+        return context.res.json({ success: false, message: 'بيانات غير صحيحة' });
     }
 
     const { email, newPassword } = payload;
 
-    if (!email || !newPassword) {
-        return context.res.json({ success: false, message: 'الإيميل والباسورد مطلوبين' });
-    }
-
     try {
-        // البحث عن المستخدم
+        // البحث عن المستخدم وتحديثه
         const userList = await users.list([sdk.Query.equal('email', email)]);
-        
-        if (userList.total === 0) {
-            return context.res.json({ success: false, message: 'المستخدم غير موجود' });
-        }
+        if (userList.total === 0) return context.res.json({ success: false, message: 'الإيميل غير مسجل' });
 
-        const userId = userList.users[0].$id;
-
-        // تحديث الباسورد بصلاحية الأدمن
-        await users.updatePassword(userId, newPassword);
-
-        return context.res.json({ success: true, message: 'تم تحديث كلمة المرور بنجاح' });
+        await users.updatePassword(userList.users[0].$id, newPassword);
+        return context.res.json({ success: true, message: 'تم التغيير بنجاح' });
     } catch (err) {
         return context.res.json({ success: false, error: err.message });
     }
